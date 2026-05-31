7970b0638e89b330f285bb8b3f00814be0add34b
Emily Ngahu <ngahuemily76@gmail.com>
2026-05-31 23:34:07 +0100

    Streamline the predict function for streamlit

--- src/predict.py
+++ src/predict.py
@@ -4,9 +4,9 @@ import numpy as np
 
 loaded_model = load_model("../model/emergency_vehicle_classifier.keras")
 
-def predict_vehicle(img_path):
+def predict_vehicle(img):
 
-    img = image.load_img(img_path, target_size=(256, 256))
+    img = img.resize((256,256))
 
     img_array = image.img_to_array(img)
 
