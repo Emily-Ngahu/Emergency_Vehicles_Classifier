d4ba13aafd66e177d6a4202c947c6ca7213b74c0
Emily Ngahu <ngahuemily76@gmail.com>
2026-05-31 23:51:10 +0100

    Creating  a simple app

---
+++ app/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+from src.predict import predict_vehicle
+
+st.title("Emergency Vehicle Classifier")
+
+uploaded_file = st.file_uploader(
+    "Upload an image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+
+    img = Image.open(uploaded_file)
+
+    st.image(img, caption="Uploaded Image")
+
+    label, probability = predict_vehicle(img)
+
+    st.success(f"Prediction: {label}")
+
+    st.write(f"Confidence: {probability:.2%}")
+    
